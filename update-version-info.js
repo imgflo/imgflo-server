fb604f463858a794a9ba1392ef026167858d29af
Jon Nordby <jononor@gmail.com>
2014-09-24 14:12:05 -0700

    Make version error non-fatal
    It looks like on Heroku the git repositories are stripped for
    git info (possibly a bare checkout), so we're unable to get this
    info :((

--- update-version-info.js
+++ update-version-info.js
@@ -2,7 +2,7 @@ require('coffee-script/register');
 var common = require('./src/common');
 common.updateInstalledVersions(function(err, path) {
     if (err) {
-        throw err;
+        console.log('ERROR:', err);
     }    
     console.log('Wrote: ' + path);
 });
