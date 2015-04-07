imgflo/imgflo-server

commit a76b8f813065efe89d759caf8b9b32571aebb92f

server: Clean up connection handling
Makes it much easier to keep this correct between
the frontend/web and worker/processing dynos

src/s3.coffee
@@ -11,16 +11,13 @@ node_static = require 'node-static'
 knox = require 'knox'
 
 class Cache extends common.CacheServer
-    constructor: (options) ->
-        defaults =
-            key: process.env.AMAZON_API_ID
-            secret: process.env.AMAZON_API_TOKEN
-            bucket: process.env.AMAZON_API_BUCKET
-            region: 'us-west-2'
-            prefix: 'test'
-        @options = common.clone defaults
-        for k,v of options
-            @options[k] = v if v
+    constructor: (config) ->
+        options =
+            key: config.cache_s3_keu
+            secret: config.cache_s3_secret
+            region: config.cache_s3_region
+            bucket: config.cache_s3_bucket
+            prefix: config.cache_s3_folder
         @client = knox.createClient @options
 
     # PERFORMANCE: keep a local index of keys?

spec/graphs.coffee
@@ -36,6 +36,14 @@ requestUrl = (testcase) ->
         u = utils.formatRequest urlbase, graph, props
     return u
 
+config =
+    workdir: './testtemp'
+    cache_local_directory: './testtemp/cache'
+    cache_type: cachetype
+    baseurl: urlbase
+    verbose: verbose
+config = require('../src/common').mergeDefaultConfig config
+
 # End-to-end tests of image processing pipeline and included graphs
 describe 'Graphs', ->
     s = null
@@ -44,12 +52,9 @@ describe 'Graphs', ->
 
     before (done) ->
         wd = './testtemp'
-        utils.rmrf wd
+        utils.rmrf config.workdir
         if startServer
-            cache =
-                type: cachetype
-                baseurl: urlbase
-            s = new server.Server wd, null, null, verbose, cache
+            s = new server.Server config
             l = new utils.LogHandler s
             s.listen urlbase, port, done
         else

spec/server.coffee
@@ -70,17 +70,21 @@ cachetype = if process.env.IMGFLO_TESTS_CACHE then process.env.IMGFLO_TESTS_CACH
 cacheurl = '/cache/' if cachetype == 'local'
 cacheurl = 'amazonaws.com' if cachetype == 's3'
 
+config =
+    workdir: './testtemp'
+    cache_local_directory: './testtemp/cache'
+    cache_type: cachetype
+    baseurl: urlbase
+    verbose: verbose
+config = require('../src/common').mergeDefaultConfig config
+
 describe 'Server', ->
     s = null
 
     before (done) ->
-        wd = './testtemp'
-        utils.rmrf wd
+        utils.rmrf config.workdir
         if startServer
-            cache =
-                type: cachetype
-                baseurl: urlbase
-            s = new server.Server wd, null, null, verbose, cache
+            s = new server.Server config
             l = new utils.LogHandler s
             s.listen urlbase, port, done
         else

spec/stress.coffee
@@ -85,6 +85,14 @@ describeTimings = (times) ->
     r['stddev-perc'] = r.stddev/r.mean*100
     return r
 
+config =
+    workdir: './testtemp'
+    cache_local_directory: './testtemp/cache'
+    cache_type: cachetype
+    baseurl: urlbase
+    verbose: verbose
+config = require('../src/common').mergeDefaultConfig config
+
 # End-to-end stress-tests of image processing server, particularly performance
 describeSkipPerformance 'Stress', ->
     s = null
@@ -94,13 +102,9 @@ describeSkipPerformance 'Stress', ->
     fs.writeFileSync outdir+'/stresstests.json', (JSON.stringify(stresstests))
 
     before (done) ->
-        wd = './testtemp'
-        utils.rmrf wd
+        utils.rmrf config.workdir
         if startServer
-            cache =
-                type: cachetype
-                baseurl: urlbase
-            s = new server.Server wd, null, null, verbose, cache
+            s = new server.Server config
             l = new utils.LogHandler s
             s.listen urlbase, port, done
         else

src/common.coffee
@@ -192,6 +192,46 @@ updateInstalledVersions = (callback) ->
               return callback err, null if err
               return callback null, p
 
+
+exports.mergeDefaultConfig = (overrides) ->
+    defaultConfig =
+        verbose: false
+        api_port: 8080
+        api_host: 'localhost:8080' # note: depends on port
+        api_key: process.env.IMGFLO_API_KEY
+        api_secret: process.env.IMGFLO_API_SECRET
+        workdir: './temp'
+        graphdir: './graphs'
+        resourcedir: './examples'
+
+        worker_type: 'internal' # will start internal worker
+        broker_url: 'direct://imgflo3'
+
+        cache_type: 'local' # will start local cache server
+        cache_s3_key: process.env.AMAZON_API_ID
+        cache_s3_secret: process.env.AMAZON_API_TOKEN
+        cache_s3_bucket: process.env.AMAZON_API_BUCKET
+        cache_s3_region: process.env.AMAZON_API_REGION
+        cache_s3_folder: 'test'
+        cache_local_directory: './cache'
+
+    config = clone defaultConfig
+    for key, value of overrides
+        config[key] = value if value
+    return config
+
+exports.getProductionConfig = () ->
+    config =
+        cache_s3_folder: 'p'
+    config.api_port = process.env.PORT if process.env.PORT?
+    config.api_host = process.env.HOSTNAME || "localhost:#{config.api_port}"
+    config.cache_type = process.env.IMGFLO_CACHE or null
+    config.worker_type = process.env.IMGFLO_WORKER or null
+    config.broker_url = process.env.CLOUDAMQP_URL or null
+    config = exports.mergeDefaultConfig config
+
+    return config
+
 exports.clone = clone
 exports.Processor = Processor
 exports.getInstalledVersions = getInstalledVersions

src/jobmanager.coffee
@@ -55,7 +55,7 @@ FrontendParticipant = (client, customId) ->
 # Performs jobs using workers over AMQP/msgflo
 class AmqpWorker extends common.JobWorker # TODO: implement
     constructor: (options) ->
-        @client = msgflo.transport.getClient options.broker
+        @client = msgflo.transport.getClient options.broker_url
         @participant = FrontendParticipant @client
         @participant.on 'data', (port, data) =>
             console.log 'participant data on port', port
@@ -84,8 +84,10 @@ class JobManager
     start: (callback) ->
         # FIXME: use msgflo transport abstraction for local versus AMQP case.
         # Still need to create participant though
-        @worker = new local.Worker @options
-        #@worker = new AmqpWorker @options
+        if @options.worker_type == 'internal'
+            @worker = new local.Worker @options
+        else
+            @worker = new AmqpWorker @options
 
         @worker.onJobUpdated = (result) =>
             @onResult result

src/local.coffee
@@ -20,7 +20,7 @@ class FsyncedWriteStream extends fs.WriteStream
 
 class Cache extends common.CacheServer
     constructor: (options) ->
-        @dir = options.directory
+        @dir = options.cache_local_directory
         if not fs.existsSync @dir
             fs.mkdirSync @dir
         defaults = {}

src/processing.coffee
@@ -44,25 +44,24 @@ waitForDownloads = (files, callback) ->
         return callback null, files
 
 class JobExecutor
-    constructor: (options) ->
+    constructor: (config) ->
 
-        @workdir = options.workdir
-        @workdir = 'temp' if not @workdir
+        @workdir = config.workdir
         if not fs.existsSync @workdir
             fs.mkdirSync @workdir
-        @graphdir = options.graphdir || './graphs'
+        @graphdir = config.graphdir
 
-        n = new noflo.Processor options.verbose
+        n = new noflo.Processor config.verbose
         @processors =
-            imgflo: new imgflo.Processor options.verbose, common.installdir
+            imgflo: new imgflo.Processor config.verbose, common.installdir
             'noflo-browser': n
             'noflo-nodejs': n
 
         if not fs.existsSync @workdir
             fs.mkdirSync @workdir
 
-        @cache = cache.fromOptions options.cache
-        @options = options
+        @cache = cache.fromOptions config
+        @options = config
 
     getGraph: (name, callback) ->
         # TODO: cache graphs

commit 3a651df4c013e39a399d36efdb67b324c7e63462

cache: Fix config/setup errors for S3 cache
Fallout from a76b8f813065efe89d759caf8b9b32571aebb92f

src/cache.coffee
@@ -8,18 +8,15 @@ common = require './common'
 local = require './local'
 s3 = require './s3'
 
-exports.fromOptions = (options) ->
+exports.fromOptions = (config) ->
 
-    defaultcacheoptions =
-        type: 'local'
-    defaultcacheoptions.directory = path.join options.workdir, 'cache' if options.workdir
-    cache = common.clone defaultcacheoptions
-    for k,v of options
-        cache[k] = v if v
-    if cache.type == 's3'
-        @cache = new s3.Cache cache
-    else if cache.type == 'local'
-        @cache = new local.Cache cache
+    type = config.cache_type
+    if type == 's3'
+        cache = new s3.Cache config
+    else if type == 'local'
+        cache = new local.Cache config
     else
-        @cache = new common.Cache cache
+        cache = new common.Cache config
+
+    return cache
 

src/s3.coffee
@@ -12,8 +12,8 @@ knox = require 'knox'
 
 class Cache extends common.CacheServer
     constructor: (config) ->
-        options =
-            key: config.cache_s3_keu
+        @options =
+            key: config.cache_s3_key
             secret: config.cache_s3_secret
             region: config.cache_s3_region
             bucket: config.cache_s3_bucket