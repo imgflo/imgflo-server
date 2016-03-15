ce5dfb9350038c10ebda8ed912f880656e4db7d1
Jon Nordby <jononor@gmail.com>
2016-03-15 19:36:15 +0100

    webpack: Also bundle most of node_modules
    Need to fix coffee-script/register dependency in msgflo and msgflo-nodejs,
    otherwise seems able to run ./build/worker.js with only:
        npm install source-map-support mqtt request websocket
    
    This is 9 MB of dependencies compared to 121 MB right now.

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,11 @@ var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
-
 module.exports = {
   module: {
 	  loaders: [
-		  { test: /\.coffee$/, loader: "coffee" }
+		  { test: /\.coffee$/, loader: "coffee" },
+      { test: /\.json$/, loader: "json" }
 	  ]
   },
   resolve: {
@@ -26,9 +18,17 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'worker.js'
   },
-  externals: nodeModules,
+  externals: {
+    'websocket': 'commonjs websocket', // has native deps, looks them up dynamically
+    'coffee-script': 'commonjs coffee-script', // should not be needed at all?
+    'mqtt': 'commonjs mqtt', // has some file with #! in it
+    'hiredis': 'commonjs hiredis', // needed by redis, could not be found
+    'vertx': 'commonjs vertx', // needed by amqplib, could not be found
+    'request': 'commonjs request', // failed with some amd define error at runtime
+  },
   plugins: [
     new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.IgnorePlugin(/\/coffee-script\//),
     new webpack.BannerPlugin('require("source-map-support").install();',
                              { raw: true, entryOnly: false })
   ],
